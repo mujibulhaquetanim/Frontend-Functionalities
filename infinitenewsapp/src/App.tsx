0e0d691b2d8b60ee4329c26805a63e44e8ee6c99
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-01-27 23:15:26 +0600

    project set up & title icon addded

---
+++ infinitenewsapp/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-6xl flex items-center justify-center">
+        Welcome to the <span className="text-red-800">`Infinite♾️News`</span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
