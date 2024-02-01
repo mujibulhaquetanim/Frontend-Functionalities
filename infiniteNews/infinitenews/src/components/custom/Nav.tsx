7abb3c1f8f34b1cd3b2d84eb06272531e0763bd8
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-02-01 23:06:49 +0600

    react-router-dom added

--- infiniteNews/infinitenews/src/components/custom/Nav.tsx
+++ infiniteNews/infinitenews/src/components/custom/Nav.tsx
@@ -1,33 +1,43 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 interface category {
   value: string;
-  setValue: (newValue: string) => void;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const Nav: React.FC<category> = ({ value, setValue }) => {
-  const arr = ["bd", "ind", "pk", "usa"];
+  const arr = [
+    "General",
+    "Business",
+    "Entertainment",
+    "Health",
+    "Science",
+    "Sports",
+    "Technology",
+  ];
   console.log(value);
 
   return (
     <div className="flex text-2xl font-bold w-full bg-green-950 text-white rounded-sm p-1">
       <div className="sticky justify-start w-[10%]">
-        <div className="bg-red-700 font-bold rounded-lg p-3 hover:animate-bounce hover:cursor-pointer inline-block">
+        <NavLink to={`/General`} className="bg-red-700 font-bold rounded-lg p-3 transition duration-150 ease-out hover:ease-in  inline-block">
           {" "}
           <span className="text-3xl">♾️</span>News{" "}
-        </div>
+        </NavLink>
       </div>
 
-      <div className="flex justify-evenly w-[90%]">
+      <div className="flex justify-between w-[90%]">
         {arr.map((item, index) => {
           return (
-            <div
-              className="border-2 rounded-lg p-2 hover:bg-red-800"
+            <NavLink
+              to={`/${item}`}
               key={index}
+              className={(isActive)=>isActive?'bg-green-900':'bg-red-800'}
               onClick={() => setValue(item)}
             >
               {item}
-            </div>
+            </NavLink>
           );
         })}
       </div>
