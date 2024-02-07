bc51ed874cb68d5eb0dbe9a21b5ce821ea9a3d97
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-02-07 21:37:32 +0600

    newsapi banned so another api on the way.

---
+++ infiniteNews/infinitenews/src/components/custom/NewsBoard.tsx
@@ -0,0 +1,14 @@
+import {useEffect} from 'react'
+
+function NewsBoard() {
+  //newsapi is banned for bangladesh. finding another api here.
+    useEffect(() =>{
+        const url;
+        fetch(url)
+    })
+  return (
+    <div>NewsBoard</div>
+  )
+}
+
+export default NewsBoard
