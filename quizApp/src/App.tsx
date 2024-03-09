08fd960864e00c1ff6c9692e681071094e9554a3
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-03-09 22:41:23 +0600

    quiz app project added.

---
+++ quizApp/src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="flex justify-center items-center font-bold text-3xl">Welcome to the quiz App</h1>
+    </>
+  )
+}
+
+export default App
