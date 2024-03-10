3d24de575f5532ed01bc30ced15d1c3fb427fc19
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-03-10 23:47:40 +0600

    Scaffolding of SearchFilter project initialized.

---
+++ SearchFilter/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="flex justify-center mt-2 p-2 items-center font-bold text-5xl">
+        Grab Your Product
+      </h1>
+    </>
+  );
+}
+
+export default App;
