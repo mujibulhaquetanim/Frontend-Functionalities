4282701a7872078fc777f032c51162b4201955e9
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-03-18 23:59:46 +0600

    Add hover effect and display text on image upload component

--- photoUpload/src/components/ImageUpload/ImageUploadNew.tsx
+++ photoUpload/src/components/ImageUpload/ImageUploadNew.tsx
@@ -6,6 +6,7 @@ function ImageUploadNew() {
   const [image, setImage] = useState(defaultImage);
   const fileRef = useRef<HTMLInputElement | null>(null);
   const [status, setStatus] = useState<boolean>(false);
+  const [showOverlay, setShowOverlay] = useState(false);
 
   const handleImageUpload = function (event: MouseEvent<HTMLButtonElement>) {
     event.preventDefault();
@@ -21,30 +22,40 @@ function ImageUploadNew() {
 
   return (
     <div className="h-screen flex items-center justify-around">
-      <div className="relative h-80 w-80 m-8">
-        <button onClick={handleImageUpload}>
-          <img
-            src={image}
-            alt="Avatar"
-            className="h-72 w-72 p-2 rounded-full hover:scale-110 hover:blur-sm"
-          />
-        </button>
-        <form id="form" encType="multipart/form-data">
-          <input
-            type="file"
-            id="file"
-            ref={fileRef}
-            onChange={UploadImageDisplay}
-            hidden
-          />
-        </form>
+      <div
+        onMouseEnter={() => setShowOverlay(true)}
+        onMouseLeave={() => setShowOverlay(false)}
+      >
+        <div className="relative h-80 w-80 m-8">
+          <button onClick={handleImageUpload}>
+            <img
+              src={image}
+              alt="Avatar"
+              className="h-72 w-72 p-2 rounded-full hover:scale-110 hover:blur-sm"
+            />
+          </button>
+          <form id="form" encType="multipart/form-data">
+            <input
+              type="file"
+              id="file"
+              ref={fileRef}
+              onChange={UploadImageDisplay}
+              hidden
+            />
+          </form>
 
-        {status === true ? (
-          <div className="font-bold text-xl justify-center flex mt-3">
-            Photo Updated 🥂
+          {status === true ? (
+            <div className="font-bold text-xl justify-center flex mt-3">
+              Photo Updated 🥂
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
+        {showOverlay && (
+          <div className="relative bottom-56 left-24   font-bold text-2xl">
+            Upload your Image
           </div>
-        ) : (
-          ""
         )}
       </div>
     </div>
