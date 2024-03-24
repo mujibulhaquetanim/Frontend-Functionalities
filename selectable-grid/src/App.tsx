aeb9af191cfff311d5ed1da56379be60941e08da
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-03-24 23:27:39 +0600

    project inited and scaffolding added

---
+++ selectable-grid/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="font-bold text-5xl text-red-900 flex justify-center m-2">
+        Select the Grid 😉
+      </div>
+    </>
+  );
+}
+
+export default App;
