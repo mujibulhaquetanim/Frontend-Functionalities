4178d8f2d030bd0e64fe1bc2704b374f60502b1b
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-24 23:00:25 +0600

    html,css & script added with some basic manipulation performed

---
+++ DOM-manipulations/src/script.js
@@ -0,0 +1,8 @@
+let headerElement = document.getElementById('header');
+
+// headerElement.innerText = 'hello';
+// headerElement.textContent = 'Hello';
+
+console.log(headerElement.innerText);
+console.log(headerElement.textContent);
+console.log(headerElement.innerHTML);
