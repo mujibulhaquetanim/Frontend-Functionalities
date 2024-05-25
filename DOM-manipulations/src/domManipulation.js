a1dfebc3c7d79c05e1b0c96915e650c49ad38001
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-25 22:46:02 +0600

    new script added and manipulated the DOM.

---
+++ DOM-manipulations/src/domManipulation.js
@@ -0,0 +1,15 @@
+const divElement = document.createElement('div');
+divElement.className = 'createdElement';
+divElement.setAttribute('id','idName');
+divElement.setAttribute('title','titleName');
+
+//inserting created div into specific position:
+const container = document.querySelector('.list');
+const h2Element= container.querySelector('h2');
+container.insertBefore(divElement, h2Element)
+//to insert it at last position:
+const a = container.appendChild(divElement); //only accepts elements, returns the element
+console.log(a);
+const b= container.append(divElement,document.createElement('p'),'Inserted using DOM'); //don't return the element and accepts multiple elements.
+console.log(b);
+
