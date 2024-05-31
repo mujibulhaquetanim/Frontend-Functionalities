6e25ddd09b64ebdc87e250f005fc2fbc0d48a53d
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-05-31 22:34:47 +0600

    bookWise dashboard project added

---
+++ DashBoard/bookwise-dashboard/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="flex justify-center items-center font-bold text-3xl">Welcome to the Bookwise Dashboard</h1>
+    </>
+  )
+}
+
+export default App
