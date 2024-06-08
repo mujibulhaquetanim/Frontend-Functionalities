b43b9089b6764f00a105f85b19afee493dc48a8f
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-08 22:56:06 +0600

    favPage,homepage updated,Default error page created

--- Medium-Large-Projects/food-recipe/src/pages/FavoritesPage.tsx
+++ Medium-Large-Projects/food-recipe/src/pages/FavoritesPage.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
-
 function FavoritesPage() {
-  const favorites = localStorage.getItem('favorite') || '[]';
+  const favorites = localStorage.getItem("favorite") || [];
   return (
     <div>
       <div>
-        <p>My Favorites:</p>
-        {favorites.length === 0 ? 'No Favorites yet' : favorites}
+        <h1 className="text-3xl font-bold m-3">My Favorites:</h1>
+        <div className="m-3">
+        {favorites.length === 0 ? "No Favorites yet" : favorites}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default FavoritesPage
+export default FavoritesPage;
---
+++ Medium-Large-Projects/food-recipe/src/pages/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+function NotFoundPage() {
+    return (
+        <div className="min-h-screen flex flex-col justify-center items-center">
+            <h1 className="text-5xl mb-3">404 Error</h1>
+            <div className="text-3xl">Not Found the Page... 🙄</div>
+        </div>
+    );
+}
+
+export default NotFoundPage;
