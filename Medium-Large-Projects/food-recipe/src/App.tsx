2b68df8ac53ecc332b12294b1b32c849b6812bca
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-08 20:35:13 +0600

    responsiveness & routes added

--- Medium-Large-Projects/food-recipe/src/App.tsx
+++ Medium-Large-Projects/food-recipe/src/App.tsx
@@ -1,10 +1,18 @@
-import './App.css'
-function App() {
+import { Routes, Route } from "react-router-dom";
+import Sidebar from "./components/SideBar";
+import HomePage from "./pages/HomePage";
+import FavoritesPage from "./pages/FavoritesPage";
 
+function App() {
   return (
-    <>
-    </>
-  )
+    <div className="flex">
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favorites" element={<FavoritesPage />} /> 
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- Medium-Large-Projects/food-recipe/src/main.tsx
+++ Medium-Large-Projects/food-recipe/src/main.tsx
@@ -2,11 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import FavoritesPage from "./pages/FavoritesPage";
+import HomePage from "./pages/HomePage";
+import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Welcome To Food Recipe Picker</div>,
+    element: <App/>,
+  },
+  {
+    path: "/favorites",
+    element: <FavoritesPage/>,
   },
 ]);
 
