15aad381e6e738c6b5373d6c9e5cfade0ef84ecc
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-15 23:20:16 +0600

    looping added for hardCoded values in the todolist to add functionalities upon them

--- DOM-manipulations/src/To-Do/script.js
+++ DOM-manipulations/src/To-Do/script.js
@@ -1,32 +1,41 @@
-let newTask= document.querySelector('#new-task');
+const { todo } = require("node:test");
+
+let newTask = document.querySelector('#new-task');
 let form = document.querySelector('form');
-let todoUl= document.querySelector('.items');
-let completeUl= document.querySelector('.complete-task ul');
+let todoUl = document.querySelector('.items');
+let completeUl = document.querySelector('.complete-task ul');
 
-let createTask = function (){
-
-}
-
-let addTask = function (){
+let createTask = function () {
 
 }
 
-let completeTask = function (){
+let addTask = function () {
 
 }
 
-let deleteTask = function(){
+let completeTask = function () {
 
 }
 
-let bindIncompleteItems = function(){
+let deleteTask = function () {
 
 }
 
-let bindCompleteItems = function (){
+let bindIncompleteItems = function () {
 
 }
 
+let bindCompleteItems = function () {
+
+}
+
+for (i = 0; i < todoUl.children.length; i++) {
+    bindIncompleteItems(todoUl.children[i], completeTask);
+}
+
+for (i = 0; i < completeUl.children.length; i++) {
+    bindCompleteItems(completeUl.children[i], deleteTask);
+}
 
 form.addEventListener('submit', addTask);
 
