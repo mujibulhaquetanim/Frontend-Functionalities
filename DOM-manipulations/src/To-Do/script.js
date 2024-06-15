4c73c2ef74c1ddf7041332204546023333eaa0c9
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-15 23:12:48 +0600

    functionalities for DOM manipulation of todo app added

--- DOM-manipulations/src/To-Do/script.js
+++ DOM-manipulations/src/To-Do/script.js
@@ -3,6 +3,30 @@ let form = document.querySelector('form');
 let todoUl= document.querySelector('.items');
 let completeUl= document.querySelector('.complete-task ul');
 
+let createTask = function (){
+
+}
+
+let addTask = function (){
+
+}
+
+let completeTask = function (){
+
+}
+
+let deleteTask = function(){
+
+}
+
+let bindIncompleteItems = function(){
+
+}
+
+let bindCompleteItems = function (){
+
+}
 
 
+form.addEventListener('submit', addTask);
 
