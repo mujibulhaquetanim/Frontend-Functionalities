60339433d6df289e2e1a7d75512feaa15fb3e57b
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-16 19:44:20 +0600

    item component added

--- Feature-Projects/ContextApiProjects/product-cart/src/App.tsx
+++ Feature-Projects/ContextApiProjects/product-cart/src/App.tsx
@@ -1,9 +1,10 @@
-
+import Items from "./components/Items"
 function App() {
 
   return (
     <>
     <header>Welcome to Aat-Baazar</header>
+    <Items name={"McBook Pro"} price={1200}/>
     </>
   )
 }
--- Feature-Projects/ContextApiProjects/product-cart/src/components/Items.tsx
+++ Feature-Projects/ContextApiProjects/product-cart/src/components/Items.tsx
@@ -1,7 +1,17 @@
-import React from 'react'
-
-export default function items() {
+import { useCart } from "./context/Cart";
+interface Items {
+  name: string;
+  price: number;
+}
+export default function Items({ name, price }: Items) {
+  const cart = useCart();
   return (
-    <div>items</div>
-  )
+    <div className="cart-item">
+      <h4>{name}</h4>
+      <p>{price}</p>
+      <button onClick={() => cart.setItems([...cart.items])}>
+        Add to Cart
+      </button>
+    </div>
+  );
 }
