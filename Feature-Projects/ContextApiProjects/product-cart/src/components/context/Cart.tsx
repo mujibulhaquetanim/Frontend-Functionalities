a5c3a5d06cda49a8ae27a785cfbde76f8c81fbe0
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-06-17 18:29:33 +0600

    interface added for items and setItems

--- Feature-Projects/ContextApiProjects/product-cart/src/components/context/Cart.tsx
+++ Feature-Projects/ContextApiProjects/product-cart/src/components/context/Cart.tsx
@@ -1,6 +1,16 @@
 import { createContext, useState, useContext } from "react";
 
-const CartContext = createContext({});
+interface items {
+  name: string;
+  price: number;
+}
+
+interface cart {
+  items: items[];
+  setItems: React.Dispatch<React.SetStateAction<items[]>>;
+}
+
+const CartContext = createContext<cart | null>(null);
 
 export const useCart = () => {
   const cart = useContext(CartContext);
@@ -8,7 +18,7 @@ export const useCart = () => {
 };
 
 export default function CartProvider(props: { children: React.ReactNode }) {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState<items[]>([]);
   return (
     <CartContext.Provider value={{ items, setItems }}>
       {props.children}
