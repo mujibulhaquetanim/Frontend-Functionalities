c8e67a3e83dd12aad505c0758ca5d58c6673618d
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-08-10 23:55:36 +0600

    navbar added for the ts-projects

--- Feature-Projects/Typescript-with-React/src/App.tsx
+++ Feature-Projects/Typescript-with-React/src/App.tsx
@@ -1,19 +1,28 @@
-import PhotoUploadingApp from "./Projects/Photo Uploading/PhotoUploadingApp"
-import DirList from "./Projects/RecurssiveFolders/DirList"
-import ScrollSpyApp from "./Projects/Scroll Spy/ScrollSpyApp"
-import TypescriptConcepts from "./Text/TypescriptConcepts"
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
 
-function App() {
-
+const Header: React.FC = () => {
   return (
-    <>
-    <DirList/>
-    <TypescriptConcepts/>
-    <ScrollSpyApp/>
-    <PhotoUploadingApp/>
-    
-    </>
-  )
-}
+    <div className="bg-slate-700 text-white p-3">
+      <nav>
+        <div className="text-xl rounded-2xl inline-block font-bold border-2 p-3 bg-slate-400 hover:bg-green-900 hover:scale-105 transform duration-300">
+          <Link to="/dirlist">Recurrsive Folders</Link>
+        </div>
 
-export default App
+        <div className="text-xl rounded-2xl inline-block font-bold border-2 p-3 bg-slate-400 hover:bg-green-900 hover:scale-105 transform duration-300">
+          <Link to="/scrollspy">Scroll Spy</Link>
+        </div>
+
+        <div className="text-xl rounded-2xl inline-block font-bold border-2 p-3 bg-slate-400 hover:bg-green-900 hover:scale-105 transform duration-300">
+          <Link to="/PhotoUploadingApp">Photo Uploading</Link>
+        </div>
+
+        <div className="text-xl rounded-2xl inline-block font-bold border-2 p-3 bg-slate-400 hover:bg-green-900 hover:scale-105 transform duration-300">
+          <Link to="/test">Test</Link>
+        </div>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
+export default Header;
--- Feature-Projects/Typescript-with-React/src/main.tsx
+++ Feature-Projects/Typescript-with-React/src/main.tsx
@@ -1,18 +1,41 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import PhotoUploadingApp from "./Projects/Photo Uploading/PhotoUploadingApp";
+import DirList from "./Projects/RecurssiveFolders/DirList";
+import ScrollSpyApp from "./Projects/Scroll Spy/ScrollSpyApp";
+import TypescriptConcepts from "./Text/TypescriptConcepts";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
+    errorElement: <div> 404 error</div>,
+    children: [
+      {
+        path: "/PhotoUploadingApp",
+        element: <PhotoUploadingApp />,
+      },
+      {
+        path: "/dirlist",
+        element: <DirList />,
+      },
+      {
+        path: "/scrollspy",
+        element: <ScrollSpyApp />,
+      },
+      {
+        path: "/test",
+        element: <TypescriptConcepts />,
+      },
+    ],
   },
-])
+]);
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
