f925d8042b48eefd43c75b090a74d4c90aff11bc
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-09 23:34:49 +0600

    separated custom components added for the board & rows

--- FunGame/tik-tac-toe/src/components/TicTacToeComponent.tsx
+++ FunGame/tik-tac-toe/src/components/TicTacToeComponent.tsx
@@ -1,20 +1,27 @@
-function board() {
-  return (
-    <div>
-
-    </div>
-  )
-}
 function TicTacToeComponent() {
   return (
     <div className="container">
       <h1>Tic Tac Toe Game</h1>
       <div className="board">
-        {board()}
+        <Board />
       </div>
       <button className="reset-button">Reset</button>
     </div>
-  )
+  );
 }
 
-export default TicTacToeComponent
+export default TicTacToeComponent;
+
+const Board = function Board() {
+  return <div>
+    <Row />
+  </div>;
+};
+
+const Row = function Row() {
+  return <div>
+    <div className="boxes"></div>
+    <div className="boxes"></div>
+    <div className="boxes"></div>
+  </div>;
+};
