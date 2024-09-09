92849bb19fd536e9d68a82e5c11cb4bba7fa7a19
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-09 23:34:11 +0600

    hardcoded user data added for tesing purposes for which authjs made cookies in the api/auth/signin route

--- authjs/auth.ts
+++ authjs/auth.ts
@@ -1,5 +1,25 @@
 import NextAuth from "next-auth";
+import Credentials from "next-auth/providers/credentials";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
-    providers: []
+    providers: [
+        Credentials({
+            async authorize(credentials) {
+                //only way login become successful is if user exists, 
+                let user = null;
+
+                user={
+                    id: 1,
+                    name: 'John Doe',
+                    email: '9Qq9Z@example.com',
+                }
+
+                if(!user) {
+                    return null
+                }
+                
+                return user
+            }
+        })
+    ]
 })
