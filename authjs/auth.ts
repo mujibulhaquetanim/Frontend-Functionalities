ae5e728a486cae66c56019827b42d2293a13d6bb
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-12 23:04:25 +0600

    [AuthJs] added default auth fields like email, password by configuring credentials inside providers array

--- authjs/auth.ts
+++ authjs/auth.ts
@@ -1,13 +1,18 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
+import email from "next-auth/providers/email";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
     providers: [
         Credentials({
+            name: 'credentials',
+            credentials: {
+                email: {label: 'Email', type: 'email'},
+                password: {label: 'Password', type: 'password'}
+            },
             async authorize(credentials) {
                 //only way login become successful is if user exists, 
                 let user = null;
-
                 user={
                     id: 1,
                     name: 'John Doe',
