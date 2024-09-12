dece3614789b19d5625d078d85b154bc736f6ff0
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-12 23:57:06 +0600

    shadcn button added & redundant line removed from auth file

--- authjs/auth.ts
+++ authjs/auth.ts
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
-import email from "next-auth/providers/email";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
     providers: [
