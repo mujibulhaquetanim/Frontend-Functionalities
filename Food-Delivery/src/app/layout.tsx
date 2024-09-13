f75083e768a192d5af587a350c75eb8d0400d663
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-13 22:56:28 +0600

    navBar & favIcon added using svg

---
+++ Food-Delivery/src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import NavBar from "@/components/NavBar";
+
+const inter = Inter({ subsets: ["latin"] });
+const favIconSvg = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍕</text></svg>`;
+
+export const metadata: Metadata = {
+  title: "YumDispatch",
+  description: "Food Delivery App",
+  icons: {
+    icon: favIconSvg,
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
