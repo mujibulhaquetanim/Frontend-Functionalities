8da989539cf2b780c3a9bda0fd9a873f412f2749
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-14 22:34:33 +0600

    redundant css removed & custom/extended css properties added in the config & global css file

--- Food-Delivery/tailwind.config.ts
+++ Food-Delivery/tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        whitish: "#fff4f2",
+        ashish: "#49557e",
+        tomato: "#ff6347",
+      },
+      fontFamily: {
+        outfit: ["var(--font-outfit)"],
       },
     },
   },
