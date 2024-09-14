79fe9157dbcd56a759a52c86fb1ebd6f2897fbe4
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-14 22:35:16 +0600

    outfit font added instead of default inter font

--- Food-Delivery/src/app/layout.tsx
+++ Food-Delivery/src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import NavBar from "@/components/NavBar";
 
-const inter = Inter({ subsets: ["latin"] });
+const fontName = Outfit({ subsets: ["latin"], variable: "--font-outfit" });
 const favIconSvg = `data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🍕</text></svg>`;
 
 export const metadata: Metadata = {
@@ -21,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${fontName.className} font-sans`}>
         <NavBar />
         {children}
       </body>
