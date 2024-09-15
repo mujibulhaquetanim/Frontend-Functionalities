baa136eebbd50406e84ff10f2fed518984aea3e0
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-16 01:48:22 +0600

    improved responsiveness of header component of home route

--- Food-Delivery/src/components/Header.tsx
+++ Food-Delivery/src/components/Header.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 export default function Header() {
   return (
-    <header className="bg-[url('/foodCollectionEid.jpg')] mt-1 bg-center bg-cover h-[34vw] relative text-lg">
-      <div className="absolute bg-black/50 max-w-[50%] text-white p-5 flex flex-col items-start gap-1 bottom-5 left-5 rounded-lg fade-in">
-        <h2 className="lg:text-7xl font-bold text-2xl">
+    <header className="bg-[url('/foodCollectionEid.jpg')] mt-1 bg-center bg-cover h-[44vh] md:h-[34vw] relative text-lg">
+      <div className="absolute bg-black/50 md:bg-black/30 shadow-2xl xl:max-w-[50%] text-white p-2 md:p-5 flex flex-col items-start gap-1 md:gap-2 bottom-[5%] left-[2%] rounded-lg fade-in">
+        <h2 className="lg:text-7xl font-bold sm:text-2xl text-base">
           Order your favorite food here
         </h2>
         <p className="text-xs lg:text-lg">
