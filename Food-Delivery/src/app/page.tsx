e2eab80703f6f2e73d0d0a6920ef16ab699807c5
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-17 23:59:58 +0600

    image added for exporemenu

--- Food-Delivery/src/app/page.tsx
+++ Food-Delivery/src/app/page.tsx
@@ -1,9 +1,11 @@
+import ExploreMenu from "@/components/ExploreMenu";
 import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <ExploreMenu />
     </main>
   );
 }
