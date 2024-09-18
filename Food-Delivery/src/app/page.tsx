76fc01a71bc3b41a59c716ca59c5746cead00fb9
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-18 22:19:42 +0600

    exploreMenu added & passed global state as props to the components.

--- Food-Delivery/src/app/page.tsx
+++ Food-Delivery/src/app/page.tsx
@@ -1,11 +1,17 @@
+"use client";
 import ExploreMenu from "@/components/ExploreMenu";
 import Header from "@/components/Header";
+import { useState } from "react";
 
 export default function Home() {
+  const [selectedMenu, setSelectedMenu] = useState("All");
   return (
-    <main>
+    <main className="space-y-3">
       <Header />
-      <ExploreMenu />
+      <ExploreMenu
+        selectedMenu={selectedMenu}
+        setSelectedMenu={setSelectedMenu}
+      />
     </main>
   );
 }
