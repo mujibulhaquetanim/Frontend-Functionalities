3c181d11582cf37a9f27529107732d8ff80963db
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-18 22:18:00 +0600

    menuImages imported and made an array of obj using image name & their imported path for rending the exploreMenu component

--- Food-Delivery/public/assets.ts
+++ Food-Delivery/public/assets.ts
@@ -1,3 +1,34 @@
+import manu_1 from '../public/menuImages/salad.png';
+import manu_2 from '../public/menuImages/noodles.png';
+import manu_3 from '../public/menuImages/burger.png';
+import manu_4 from '../public/menuImages/pasta.jpg';
+import manu_5 from '../public/menuImages/vegetarian food.png';
+import manu_6 from '../public/menuImages/seafood.png';
+import manu_7 from '../public/menuImages/dessert.jpg';
+import manu_8 from '../public/menuImages/rolls.jpg';
+import manu_9 from '../public/menuImages/pizza.png';
+import manu_10 from '../public/menuImages/sandwich.png';
 
 
-export const menu_list = []
+export const menu_list = [
+    {menu_name: 'Salad',
+    menu_image: manu_1},
+    {menu_name: 'Noodles',
+    menu_image: manu_2},
+    {menu_name: 'Burger',
+    menu_image: manu_3},
+    {menu_name: 'Pasta',
+    menu_image: manu_4},
+    {menu_name: 'Pure Vegetarian',
+    menu_image: manu_5},
+    {menu_name: 'Seafood',
+    menu_image: manu_6},
+    {menu_name: 'Dessert',
+    menu_image: manu_7},
+    {menu_name: 'Rolls',
+    menu_image: manu_8},
+    {menu_name: 'Pizza',
+    menu_image: manu_9},
+    {menu_name: 'Sandwich',
+    menu_image: manu_10},
+]
