9c8cb4c797e405fe48b69dc88feeeab355e57ee2
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-21 23:13:21 +0600

    useStoreContext hook used in the foodDelivery component

--- Food-Delivery/src/components/FoodDisplay.tsx
+++ Food-Delivery/src/components/FoodDisplay.tsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { food_list } from "../../public/assets";
+import { StaticImageData } from "next/image";
+import { useStoreContext } from "@/context/StoreContext";
 
+interface foodList {
+  menu_name: string;
+  menu_image: StaticImageData;
+  items: {
+    _id: number;
+    foodName: string;
+    foodImage: string;
+    foodPrice: number;
+    description: string;
+    category: string;
+  }[];
+}
+[];
 export default function FoodDisplay({
   selectedMenu,
 }: {
   selectedMenu: string;
 }) {
+  const { menu_list } = useStoreContext();
   return (
     <div>
       <h2>Top Picks nearby</h2>
