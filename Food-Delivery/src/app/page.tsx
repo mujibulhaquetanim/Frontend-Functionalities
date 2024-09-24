d4898627948dfb996e0f845421497b9b8feb0153
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-24 22:11:05 +0600

    used grid for utmost responsiveness for min-screen size.

--- Food-Delivery/src/app/page.tsx
+++ Food-Delivery/src/app/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import ExploreMenu from "@/components/ExploreMenu";
-import FoodDisplay from "@/components/FoodDisplay";
+import FoodDisplay from "@/components/foodDisplay/FoodDisplay";
 import Header from "@/components/Header";
 import { useState } from "react";
 
 export default function Home() {
   const [selectedMenu, setSelectedMenu] = useState("All");
   return (
-    <main className="space-y-3">
+    <main className="grid gap-3">
       <Header />
       <ExploreMenu
         selectedMenu={selectedMenu}
