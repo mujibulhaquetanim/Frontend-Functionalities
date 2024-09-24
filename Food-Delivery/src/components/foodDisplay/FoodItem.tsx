8d00e2e338eea1314971dc8e2ce019404dd9a6ac
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-24 22:53:19 +0600

    foodItem component added to display each item within a card

---
+++ Food-Delivery/src/components/foodDisplay/FoodItem.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Image, { StaticImageData } from "next/image";
+import React from "react";
+
+interface FoodItemProps {
+  foodName: string;
+  foodImage: StaticImageData;
+  foodPrice: number;
+  description: string;
+  category: string;
+}
+
+export default function FoodItem({
+  foodName,
+  foodImage,
+  foodPrice,
+  description,
+  category,
+}: FoodItemProps) {
+  return (
+    <div className="m-3 border-2 p-3 rounded-2xl">
+      <div className="mb-1">
+        <Image className="object-cover aspect-video rounded-md" src={foodImage} alt={foodName} />
+      </div>
+      <div className="space-y-1">
+      <h1><span className="font-bold text-tomato">{foodName}</span></h1>
+        <p className="line-clamp-1 text-grayish"><span className=""></span> {description}</p>
+        <p className="flex gap-3"><span className="underline">Price:</span><span className="text-tomato font-bold">${foodPrice}</span></p>
+        <p className="flex gap-3"><span className="underline">Category:</span> <span className="font-bold text-red-900">{category}</span></p>
+      </div>
+    </div>
+  );
+}
