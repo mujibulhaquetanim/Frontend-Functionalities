5208c41b0da1d7ff364ea4c49df4b84c1b3b9be8
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-09-30 23:24:08 +0600

    api & middleware added to the store for handing api & enabling caching with other features

--- rtk-query/src/States/store.ts
+++ rtk-query/src/States/store.ts
@@ -1,11 +1,19 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterSlice from "./Counter/CounterSlice";
+import { postApiSlice } from "./posts/PostApiSlice";
 
 export const store = configureStore({
     reducer: {
         counter: counterSlice,
-    }
+        [postApiSlice.reducerPath]: postApiSlice.reducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(postApiSlice.middleware), 
 })
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+/*
+* Add the API reducer to the store.
+* Adding the api middleware enables caching, invalidation, polling, and other features of RTK Query
+*/
