9dea451f4e7cede53b0f921573e69970c4578dbb
Mujibul Haque Tanim <mujibultanim@gmail.com>
2024-11-26 12:52:31 +0600

    images for shopping_cart + made them exportable to use in the react component to render them

---
+++ Medium-Large-Projects/shopping-cart/src/assets/productList.ts
@@ -0,0 +1,52 @@
+import Burger from "./public/burger.png";
+import Pizza from "./public/pizza.png";
+import Dessert from "./public/dessert.jpg";
+import Salad from "./public/salad.png";
+import Rolls from "./public/rolls.jpg";
+import Nodddles from "./public/noodles.png";
+import Pasta from "./public/pasta.jpg";
+
+export const productList = [
+    {
+        id: 1,
+        name: "Burger",
+        image: Burger,
+        price: 10
+    },
+    {
+        id: 2,
+        name: "Pizza",
+        image: Pizza,
+        price: 10
+    },
+    {
+        id: 3,
+        name: "Dessert",
+        image: Dessert,
+        price: 10
+    },
+    {
+        id: 4,
+        name: "Salad",
+        image: Salad,
+        price: 10
+    },
+    {
+        id: 5,
+        name: "Rolls",
+        image: Rolls,
+        price: 10
+    },
+    {
+        id: 6,
+        name: "Noodles",
+        image: Nodddles,
+        price: 10
+    },
+    {
+        id: 7,
+        name: "Pasta",
+        image: Pasta,
+        price: 10
+    },
+    ]
