c1c63c20d50899efe5bcb6eb7b609f3a62716e3f
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-09 22:09:26 +0600

    navbar created + routes added

---
+++ Portfolios/mujibv.1/src/components/nav-components/Home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="h-full flex items-center justify-center border bg-slate-300">
+      <h1>Home</h1>
+    </div>
+  );
+}
--- Portfolios/mujibv.1/src/main.tsx
+++ Portfolios/mujibv.1/src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App.tsx";
+import "./index.css";
+import Home from "./components/nav-components/Home.tsx";
+import Projects from "./components/Projects.tsx";
+import Blogs from "./components/Blogs.tsx";
+import DashBoard from "./components/DashBoard.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="Projects" element={<Projects />} />
+          <Route path="Blogs" element={<Blogs />} />
+          <Route path="dashboard" element={<DashBoard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
