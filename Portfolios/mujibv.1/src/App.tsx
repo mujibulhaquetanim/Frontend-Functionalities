ffc6ee3b2c5a023e11ab46acb33351951b74e659
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-09 22:12:22 +0600

    home page and its children created using component based method + col-span used for grid designs.

--- Portfolios/mujibv.1/src/App.tsx
+++ Portfolios/mujibv.1/src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
+import { Outlet } from "react-router";
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
-    <>
-      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-    </>
-  )
+    <div className="flex h-screen bg-slate-500">
+      <div className="grid grid-cols-12 h-full w-full">
+        <div className="grid col-span-1">
+          <NavBar />
+        </div>
+
+        <div className="grid col-span-11">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
