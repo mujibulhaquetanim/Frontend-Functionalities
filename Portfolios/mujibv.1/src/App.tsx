a2dff901b96632df028b7b0f3ae311f771de94b6
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-11 14:10:49 +0600

    conditional rendering added. now navbar won't be available for the homepage.

--- Portfolios/mujibv.1/src/App.tsx
+++ Portfolios/mujibv.1/src/App.tsx
@@ -1,15 +1,23 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import NavBar from "./components/NavBar";
 
 function App() {
+  const location = useLocation();
+  const homePage = location.pathname === "/";
   return (
     <div className="flex h-screen bg-slate-500">
       <div className="grid grid-cols-12 h-full w-full">
-        <div className="grid col-span-1">
-          <NavBar />
-        </div>
+        {!homePage && (
+          <div className="grid col-span-1">
+            <NavBar />
+          </div>
+        )}
 
-        <div className="grid col-span-11">
+        <div
+          className={`${
+            homePage ? "col-span-12" : "col-span-10"
+          } grid col-span-11`}
+        >
           <Outlet />
         </div>
       </div>
