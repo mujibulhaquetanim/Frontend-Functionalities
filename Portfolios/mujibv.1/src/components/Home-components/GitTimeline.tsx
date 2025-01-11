91234440497316ff7476dad2c1ea5e021c09b4e0
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-11 14:15:01 +0600

    boilerplate code added for the structure design. will be implemented now on

---
+++ Portfolios/mujibv.1/src/components/Home-components/GitTimeline.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function GitTimeline() {
+  return (
+    <div>GitTimeline</div>
+  )
+}
---
+++ Portfolios/mujibv.1/src/components/Home-components/TypingSpeedIndicator.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function TypingSpeedIndicator() {
+  return (
+    <div>TypingSpeedIndicator</div>
+  )
+}
