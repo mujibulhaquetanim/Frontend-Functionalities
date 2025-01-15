022cfebcf7280672cf657ccba90bddeac14d6743
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-15 23:45:01 +0600

    automatic redirection added for the project route

--- Portfolios/mujibv.1/src/main.tsx
+++ Portfolios/mujibv.1/src/main.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import App from "./App.tsx";
 import "./index.css";
 import Home from "./components/nav-components/Home.tsx";
-import Projects from "./components/Projects.tsx";
+import Projects from "./components/Projects/Projects.tsx";
 import Blogs from "./components/Blogs.tsx";
 import DashBoard from "./components/DashBoard.tsx";
 import AboutMe from "./components/Home-components/AboutMe.tsx";
+import Frontend from "./components/Projects/Frontend.tsx";
+import Backend from "./components/Projects/Backend.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -15,8 +17,13 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
-          <Route path="Projects" element={<Projects />} />
-          <Route path="Blogs" element={<Blogs />} />
+          <Route path="projects" element={<Projects />}>
+            {/* default route and redirect */}
+            <Route index element={<Navigate to="frontend" replace />} />
+            <Route path="frontend" element={<Frontend />} />
+            <Route path="backend" element={<Backend />} />
+          </Route>
+          <Route path="blogs" element={<Blogs />} />
           <Route path="dashboard" element={<DashBoard />} />
           <Route path="about" element={<AboutMe />} />
         </Route>
