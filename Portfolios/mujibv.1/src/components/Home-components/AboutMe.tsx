5a7929f2cde94adaab1d4b1a7d0a8bf9bd05c8de
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-18 21:53:49 +0600

    home page components made mobile friendly and added button to see more infos.

--- Portfolios/mujibv.1/src/components/Home-components/AboutMe.tsx
+++ Portfolios/mujibv.1/src/components/Home-components/AboutMe.tsx
@@ -1,12 +1,14 @@
 import { NavLink } from "react-router";
+import React from "react";
 
 export default function AboutMe() {
+  const [show, setShow] = React.useState(false);
   return (
     <div className="min-h-screen backdrop-blur-xl grid grid-flow-col place-items-center place-content-center">
-      <div className="grid place-items-center place-content-center grid-cols-12 grid-rows-8 gap-2">
-        <div className="col-span-6 row-span-8 border-2 border-slate-700/80 bg-slate-700/80 text-white backdrop-blur-xl rounded-xl p-5 space-y-5 text-justify">
+      <div className="grid place-items-center place-content-center grid-cols-12 grid-rows-8 gap-2 m-3 md:m-0">
+        <div className="md:col-span-6 md:row-span-8 col-span-12 row-span-12 border-2 border-slate-700/80 bg-slate-600/80 text-white backdrop-blur-xl rounded-xl p-5 space-y-5 text-justify">
           <h1 className="text-3xl font-bold underline">About Me</h1>
-          <div className=" space-y-3">
+          <div className="hidden md:block space-y-3">
             <p>
               👋 Hi, I'm Mujibul Haque Tanim, a passionate Computer Science
               graduate with a strong foundation in software engineering and data
@@ -21,7 +23,6 @@ export default function AboutMe() {
               JavaScript, taking on challenging projects that sharpened my
               technical skills and problem-solving abilities.{" "}
             </p>
-
             <p>
               💻 Over time, I developed expertise in front-end development using
               Next.js and React, focusing on building dynamic, responsive user
@@ -54,9 +55,76 @@ export default function AboutMe() {
               enhanced my ability to present ideas clearly and visually.
             </p>
           </div>
+          {/* Mobile View */}
+          <div className="md:hidden space-y-3">
+            <p>
+              👋 Hi, I'm Mujibul Haque Tanim, a passionate Computer Science
+              graduate with a strong foundation in software engineering and data
+              science. Throughout my degree, I cultivated a deep curiosity for
+              programming, evolving into a commitment to mastering front-end,
+              back-end, and data-driven solutions that make an impact.
+            </p>
+            <p>
+              {" "}
+              🎓 During my time as a Computer Science and Engineering student, I
+              specialized in core programming languages like C, Java, and
+              JavaScript, taking on challenging projects that sharpened my
+              technical skills and problem-solving abilities.{" "}
+            </p>
+            <button
+              className={
+                show
+                  ? "hidden"
+                  : "border px-3 py-1 rounded-xl bg-slate-400 hover:bg-slate-700 hover:scale-105 transform duration-300"
+              }
+              onClick={() => setShow(!show)}
+            >
+              Read More...
+            </button>
+            {show ? (
+              <div>
+                {" "}
+                <p>
+                  💻 Over time, I developed expertise in front-end development
+                  using Next.js and React, focusing on building dynamic,
+                  responsive user interfaces. On the back end, I emphasize
+                  security and scalability, working with NestJS and Golang to
+                  design clean architectures while adhering to industry best
+                  practices.{" "}
+                </p>
+                <p>
+                  🔧 I also have strong experience in API design, ensuring
+                  well-structured and reliable systems. My toolkit includes Git,
+                  Docker, Redis, DBMS, NGINX, Kafka, and message queues, which I
+                  use to streamline development and optimize infrastructure.{" "}
+                </p>
+                <p>
+                  {" "}
+                  📊 My skills in Python for data analysis, using tools like
+                  NumPy and Pandas, allow me to extract valuable insights from
+                  complex datasets. My thesis on predictive modeling applied
+                  advanced ensemble techniques and BERT-based NLP methods,
+                  demonstrating my ability to solve real-world problems with
+                  innovative data-driven solutions.{" "}
+                </p>
+                <p>
+                  🌱 Lifelong learning is at the heart of my approach. I
+                  continuously expand my knowledge through self-directed
+                  learning, online resources, and courses to stay at the cutting
+                  edge of technology. Additionally, I’ve explored my creative
+                  side with tools like Figma, Excalidraw, Photoshop, and Da
+                  Vinci, which have enhanced my ability to present ideas clearly
+                  and visually.
+                </p>
+              </div>
+            ) : (
+              ""
+            )}
+          </div>
         </div>
 
-        <div className="col-span-6 row-span-4 border-2 border-slate-700/80 bg-slate-700/80 text-white backdrop-blur-xl rounded-xl p-5 space-y-5">
+        {/* Education */}
+        <div className="md:col-span-6 md:row-span-4 col-span-12 row-span-12 border-2 border-slate-600/80 bg-slate-600/80 text-white backdrop-blur-xl rounded-xl p-5 space-y-5">
           <div className="text-2xl underline font-bold">Education</div>
           <div>
             <h1 className="text-xl">
@@ -90,7 +158,8 @@ export default function AboutMe() {
           </div>
         </div>
 
-        <div className="text-xl col-span-6 row-span-4 border-2 border-slate-700/80 bg-slate-700/80 text-white backdrop-blur-xl rounded-xl p-5">
+        {/* Timeline */}
+        <div className="text-xl md:col-span-6 md:row-span-4 col-span-12 row-span-12 border-2 border-slate-700/80 bg-slate-700/80 text-white backdrop-blur-xl rounded-xl p-5">
           timeline
         </div>
       </div>
