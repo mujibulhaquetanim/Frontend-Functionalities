e559a86bb6294155ab9c6117caa725dfdc3fb9cd
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-18 23:02:23 +0600

    made navbar responsive for the smaller display using absolute property

--- Portfolios/mujibv.1/src/App.tsx
+++ Portfolios/mujibv.1/src/App.tsx
@@ -6,9 +6,15 @@ function App() {
   const homePage = location.pathname === "/";
   return (
     <div className="grid min-h-screen bg-slate-700">
-      <div className="md:grid md:grid-cols-12">
+      <div className="md:grid md:grid-cols-12 relative">
         {!homePage && (
-          <div className="hidden md:grid md:col-span-1">
+          <div
+            className="
+        absolute z-10 bottom-10 left-[50%] right-[50%] translate-x-[-50%] translate-y-[50%]
+        md:static md:translate-x-0 md:translate-y-0
+        md:grid md:col-span-1
+      "
+          >
             <NavBar />
           </div>
         )}
--- Portfolios/mujibv.1/src/components/NavBar.tsx
+++ Portfolios/mujibv.1/src/components/NavBar.tsx
@@ -4,14 +4,14 @@ import { MdOutlineDashboard } from "react-icons/md";
 
 export default function NavBar() {
   const routes = [
-    { to: "/", icon: <FaHome color="white"  size={20}/> },
+    { to: "/", icon: <FaHome color="white" size={20} /> },
     { to: "/Projects", icon: <FaFolder color="white" size={20} /> },
-    { to: "/Blogs", icon: <FaRss color="white"  size={20}/> },
+    { to: "/Blogs", icon: <FaRss color="white" size={20} /> },
     { to: "/dashboard", icon: <MdOutlineDashboard color="white" size={20} /> },
   ];
   return (
     <nav className="grid place-items-center place-content-center h-full drop-shadow-lg gap-3">
-      <div className="border-2 py-7 px-5 rounded-2xl gap-7 grid bg-slate-700">
+      <div className=" border-2 py-7 px-5 rounded-2xl gap-7 grid md:grid-flow-row grid-flow-col bg-slate-700">
         {routes.map((route, index) => {
           return (
             <NavLink
