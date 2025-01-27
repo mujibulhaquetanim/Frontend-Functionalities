4bf6669c8e61413b3b4a0a4af6c1209b2bec91ba
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-27 22:49:09 +0600

    types added for the fetch data.

--- Query/tanstack-query/src/axios/todoFetching.ts
+++ Query/tanstack-query/src/axios/todoFetching.ts
@@ -1,6 +1,7 @@
 import { axiosInstance } from "./axiosInstance";
+import { Itodo } from "../types/todoTypes";
 
-export const getTodos = async () => {
-    const response = await axiosInstance.get('/todos');
-    return response.data;
+export const getTodos = async (): Promise<Itodo[]> => {
+    const response = await axiosInstance.get<Itodo[]>('/todos');
+    return response.data;   
 }
