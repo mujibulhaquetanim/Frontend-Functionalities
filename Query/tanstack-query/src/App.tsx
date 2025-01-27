e622487e2e579f1fc2756c09cd7094c04c9e5474
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-27 22:50:33 +0600

    styling added and useSuspenseQuery added for assert known types which won't give error for probable undefined types

--- Query/tanstack-query/src/App.tsx
+++ Query/tanstack-query/src/App.tsx
@@ -1,15 +1,37 @@
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import "./App.css";
 import { createTodoQueryOptions } from "./queryOptions/createTodoQueryOptions";
 
 function App() {
-  const { data, isPending, isError } = useQuery(createTodoQueryOptions());
+  const { data, isPending, isError } = useSuspenseQuery(
+    createTodoQueryOptions()
+  );
 
   return (
     <>
       <div>Tanstack Query Example</div>
       {/* json.stringify will convert the array to string, if we don't use it then we will get the array in the form of object which is not readable and cannot be displayed  */}
-      <div>{isPending ? "Loading..." : JSON.stringify(data?.slice(0, 10))}</div>
+      {/* <div>{isPending ? "Loading..." : JSON.stringify(data?.slice(0, 10))}</div> */}
+      <div>
+        {isPending
+          ? "Loading..."
+          : data?.slice(0, 10).map((todo) => (
+              <div key={todo.id} style={{ marginBottom: "10px" }}>
+                <span
+                  style={{
+                    color: "red",
+                    marginRight: "10px",
+                    fontWeight: "bold",
+                    textDecoration: "underline",
+                    padding: "5px",
+                  }}
+                >
+                  Todo name:
+                </span>
+                {todo.title}
+              </div>
+            ))}
+      </div>
       {isError && <div>Error</div>}
     </>
   );
