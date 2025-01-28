f742713636f77270cc26582a058c24756c1b571c
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-28 22:58:02 +0600

    error added for tanstack query + styles added to the heading

--- Query/tanstack-query/src/App.tsx
+++ Query/tanstack-query/src/App.tsx
@@ -3,13 +3,13 @@ import "./App.css";
 import { createTodoQueryOptions } from "./queryOptions/createTodoQueryOptions";
 
 function App() {
-  const { data, isPending, isError } = useSuspenseQuery(
+  const { data, isPending, isError, error } = useSuspenseQuery(
     createTodoQueryOptions()
   );
 
   return (
     <>
-      <div>Tanstack Query Example</div>
+      <div style={{ textAlign: "center", fontSize: "2rem", textDecoration: "underline", marginBottom: "10px", fontWeight: "bold" }}>Tanstack Query Example</div>
       {/* json.stringify will convert the array to string, if we don't use it then we will get the array in the form of object which is not readable and cannot be displayed  */}
       {/* <div>{isPending ? "Loading..." : JSON.stringify(data?.slice(0, 10))}</div> */}
       <div>
@@ -32,7 +32,7 @@ function App() {
               </div>
             ))}
       </div>
-      {isError && <div>Error</div>}
+      {isError && <div>Error: {(error as Error).message}</div>}
     </>
   );
 }
