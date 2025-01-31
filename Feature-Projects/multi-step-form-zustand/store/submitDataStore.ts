0c1961a501100be2073868a3dccb526f7982b11c
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-31 23:32:39 +0600

    user data will be persisted in the localStorage using zustand middleware's persist, storage properties

--- Feature-Projects/multi-step-form-zustand/store/submitDataStore.ts
+++ Feature-Projects/multi-step-form-zustand/store/submitDataStore.ts
@@ -1,14 +1,24 @@
 import {onboardingSchema} from '@/types/schema';
 import {create} from 'zustand';
+import {createJSONStorage, persist} from 'zustand/middleware'
 
 //partial is used as the data will be stored step by step not all at once as it is a multi-step form.
 type OnBoardingState = Partial<onboardingSchema> & {
     setData: (data: Partial<onboardingSchema>)=> void;
 }
 
-export const useSubmitDataStore = create<OnBoardingState>((set)=>{
-    return {
-        // set will be used to update the state and setData will be used to update the state.
+export const useSubmitDataStore = create<OnBoardingState>()(
+    persist((set)=>({
         setData: (data)=>set(data),
-    }
-})
+    }), {
+        name: 'onboarding-data',
+        storage: createJSONStorage(()=>localStorage)
+    })
+)
+
+// export const useSubmitDataStore = create<OnBoardingState>((set)=>{
+//     return {
+//         // set will be used to update the state and setData will be used to update the state.
+//         setData: (data)=>set(data),
+//     }
+// })
