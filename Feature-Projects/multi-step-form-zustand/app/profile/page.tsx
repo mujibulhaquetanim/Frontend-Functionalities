676fe25332275f3e98481370248ddbecd0b15c4a
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-01-31 23:31:33 +0600

    profile page added which will showcase the user information of the multi-step form.

--- Feature-Projects/multi-step-form-zustand/app/profile/page.tsx
+++ Feature-Projects/multi-step-form-zustand/app/profile/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { useSubmitDataStore } from "@/store/submitDataStore";
 
 export default function page() {
+  const { setData, ...state } = useSubmitDataStore();
   return (
     <div>
-      <h1>Profile</h1>
+      <h1 className="text-3xl font-bold">Account Created</h1>
+      <p className="text-lg">
+        Thank you for creating an account. You can now log in.
+      </p>
+      <p className="text-lg">Your Profile information are:</p>
+      <p className="text-lg">Username: {state.username}</p>
+      <p className="text-lg">Name: {state.firstName}</p>
+      <p className="text-lg">Email: {state.lastName}</p>
+      <p className="text-lg">Password: {state.password}</p>
     </div>
-  )
+  );
 }
