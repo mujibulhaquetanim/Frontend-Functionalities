a521961829e614b6d38b36ad999b456b27718041
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-06 22:01:00 +0600

    authActions like signIn, signOut for the credential login created with proper error check using `authError` of next-auth.

---
+++ authjs/src/app/actions/authActions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import {signIn, signOut} from '../../../auth';
+import { AuthError } from 'next-auth';
+
+export async function handleCredentialSignIn({email, password}:{email: string, password: string}) {
+    try {
+        await signIn('credentials', {email, password, redirectTo: '/'});
+    } catch (error) {
+        if (error instanceof AuthError){
+            switch (error.type){
+                case 'CredentialsSignin':
+                    return {
+                        message: 'Invalid email or password'
+                    }
+                default:
+                    return {
+                        message: 'Something went wrong. Please try again later.'
+                    }
+            }
+        }
+        throw error;
+    }
+}
+
+export async function handleSignOut() {
+    await signOut();
+}
