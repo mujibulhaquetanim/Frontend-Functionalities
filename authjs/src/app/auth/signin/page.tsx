b570aa3e027785a215f720ea1e197e1075068b3e
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-06 22:12:07 +0600

    signIn page created with form and signIn action and loadingButton + errorMessage components added

--- authjs/src/app/auth/signin/page.tsx
+++ authjs/src/app/auth/signin/page.tsx
@@ -1,8 +1,91 @@
-'use client'
-import React from 'react'
+"use client";
 
-export default function SignIn() {
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import LoadingButton from "@/components/LoadingButton";
+import { signinSchema } from "@/schema/signinSchema";
+import { handleCredentialSignIn } from "@/app/actions/authActions";
+import { useState } from "react";
+import ErrorMessage from "@/components/error-message";
+
+export function Signin() {
+  const [globalError, setGlobalError] = useState<string>("");
+  const form = useForm<z.infer<typeof signinSchema>>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: z.infer<typeof signinSchema>) {
+    try {
+      const result = await handleCredentialSignIn(values);
+      if (result?.message) {
+        setGlobalError(result.message);
+      }
+    } catch (error) {
+      console.log("An unexpected error occured. Please try again.");
+    }
+  }
+
   return (
-    <div>page</div>
-  )
+    <div>
+      {globalError && <ErrorMessage error={globalError} />}
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="enter your email"
+                    autoComplete="off"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" placeholder="********" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This is your public display name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <LoadingButton pending={form.formState.isSubmitting} />
+        </form>
+      </Form>
+    </div>
+  );
 }
