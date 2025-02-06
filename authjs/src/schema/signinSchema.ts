0a5dedb9e20260d4900f986c19a05ad71b6c5166
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-06 22:06:40 +0600

    validation schema created for the signIn page using zod.

---
+++ authjs/src/schema/signinSchema.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+export const signinSchema = z.object({
+  email: z
+    .string()
+    .email({
+      message: "Please enter a valid email address",
+    })
+    .min(3, {
+      message: "Email must be at least 3 characters",
+    }),
+  password: z
+    .string({
+      message: "Please enter a valid password",
+    })
+    .min(7, {
+      message: "Password must be at least 7 characters",
+    })
+    .max(20, {
+      message: "Password must be less than 20 characters",
+    }),
+});
