25a63dfcc120da69bd6f2da3a299a3d99294725f
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-08 22:43:16 +0600

    github provider added in the auth file.

--- authjs/auth.ts
+++ authjs/auth.ts
@@ -5,7 +5,17 @@ import Github    from "next-auth/providers/github";
 
 export const {handlers, signIn, signOut, auth} = NextAuth({
     providers: [
-        Github,
+        Github({
+            profile(profile){
+                // this function is called when user logs in and we get the profile of user which returns an object with id, name and email, we use this object to store user in our database.
+                return {
+                    //make sure to convert id to string because id is of type bigint
+                    id: profile.id.toString(),
+                    name: profile.name,
+                    email: profile.email
+                }
+            }
+        }),
         Credentials({
             name: 'credentials',
             credentials: {
@@ -51,14 +61,17 @@ export const {handlers, signIn, signOut, auth} = NextAuth({
             //redirect user to login page if they are not logged in
             return !!auth
         },
+
+        //this function is called when a token is created or updated, when user logins we get access token and user object. user object is from credentials provider (authorize function)
         jwt({token, user}){
             if(user){
                 token.user = user
             }
             return token
         },
+        //it is executed whenever we try to access token
         session({session, token}){
-            session.user.id = token.id;
+            session.user.id = token.id
             return session;
         }
     },
