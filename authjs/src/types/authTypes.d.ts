38f6fe598761704f1f15444a6c9dba3f283b21b7
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-09 22:26:24 +0600

    role added in the authTypes

--- authjs/src/types/authTypes.d.ts
+++ authjs/src/types/authTypes.d.ts
@@ -4,6 +4,7 @@ import 'next-auth/jwt'
 declare module "next-auth" {
     interface User{
         id: string  
+        role: string
     }
 
     interface Session{
@@ -14,5 +15,6 @@ declare module "next-auth" {
 declare module "next-auth/jwt"{
     interface JWT{
         id: string
+        role: string
     }
 }
