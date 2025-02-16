61016357978210311ca5bbbb4fa579eb4dff0b85
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-02-16 23:32:56 +0600

    custom hook added and passed the commentData as parentTree, user interacted id and updated comment made by user.

--- Feature-Projects/infinite-nested-components/src/App.tsx
+++ Feature-Projects/infinite-nested-components/src/App.tsx
@@ -1,4 +1,5 @@
 import Comment from './components/Comment.tsx';
+import useFunctions from './components/useAddComment.tsx';
 import { commentData } from './data/CommentsData.ts';
 import { useState } from 'react';
 
@@ -10,9 +11,13 @@ interface CommentType {
 
 function App() {
   const [comment, setComment] = useState<CommentType>(commentData);
+  const {addComments} = useFunctions();
 
   const handleComments = (commentId: string, newComment: CommentType)=>{
-    console.log(commentId, newComment, 'handleComments');
+    // console.log(commentId, newComment, 'handleComments');
+    const updatedComment = addComments(comment, commentId, newComment);
+    setComment(updatedComment)
+
   }
   
   return (
