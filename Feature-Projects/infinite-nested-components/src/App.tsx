82670db2dcaf8c7c8b8be80cf7281c7b423b6fbc
Mujibul Haque Tanim <mujibultanim@gmail.com>
2025-04-05 22:39:03 +0600

    [nested comment] add `textarea` for adding new comment

--- Feature-Projects/infinite-nested-components/src/App.tsx
+++ Feature-Projects/infinite-nested-components/src/App.tsx
@@ -1,7 +1,7 @@
-import Comment from './components/Comment.tsx';
-import useFunctions from './components/useAddComment.tsx';
-import { commentData } from './data/CommentsData.ts';
-import { useState } from 'react';
+import Comment from "./components/Comment.tsx";
+import useFunctions from "./components/useAddComment.tsx";
+import { commentData } from "./data/CommentsData.ts";
+import { useState } from "react";
 
 interface CommentType {
   id: string;
@@ -10,21 +10,47 @@ interface CommentType {
 }
 
 function App() {
+  const [newComment, setNewComment] = useState("");
   const [comment, setComment] = useState<CommentType>(commentData);
-  const {addComments} = useFunctions();
+  const { addComments } = useFunctions();
 
-  const handleComments = (commentId: string, newComment: CommentType)=>{
+  const handleComments = (commentId: string, newComment: CommentType) => {
     // console.log(commentId, newComment, 'handleComments');
     const updatedComment = addComments(comment, commentId, newComment);
-    setComment(updatedComment)
+    setComment(updatedComment);
+  };
 
-  }
-  
+  const handleSubmit = () => {
+    if (newComment) {
+      //logic
+    }
+  };
+
   return (
     <>
-      <Comment key={comment.id} data={comment} handleComments={handleComments}/>
+      <div className="flex flex-col gap-4">
+        <textarea
+          value={newComment}
+          rows={3}
+          cols={50}
+          onChange={(e) => setNewComment(e.target.value)}
+          placeholder="Write your comment"
+          className="w-1/2 p-4 self-center m-3 "
+        />
+        <button
+          className="bg-blue-500 rounded-full px-4 py-2 w-1/12 self-center text-white"
+          onClick={handleSubmit}
+        >
+          Add Comment
+        </button>
+      </div>
+      <Comment
+        key={comment.id}
+        data={comment}
+        handleComments={handleComments}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
