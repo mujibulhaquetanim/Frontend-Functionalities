c2f9dd6ec5becafd2963b4f37fd6277330c09df1
mujibulhaquetanim <mujibultanim@gmail.com>
2025-05-05 00:17:08 +0600

    `fix:` update image source paths in dice roller and correct heading capitalization.

--- DOM-manipulations/Projects/DiceRoller/dice.js
+++ DOM-manipulations/Projects/DiceRoller/dice.js
@@ -5,10 +5,11 @@ function rollDice() {
     const img = [];
     const diceArr = [];
 
+    // loop will run numOfDice times and push random number between 1 to 6
     for (let i = 1; i <= numOfDice; i++) {
         const diceRoll = Math.floor(Math.random() * 6) + 1;
         diceArr.push(diceRoll);
-        img.push(`<img src="dice${diceRoll}.png" alt="dice${diceRoll}">`);
+        img.push(`<img src="./images/${diceRoll}.png" width="100px" height="100px" id="diceImages" alt="dice${diceRoll}">`);
     }
     diceNum.textContent = diceArr.join(', ');
     diceImg.innerHTML = img.join('');
