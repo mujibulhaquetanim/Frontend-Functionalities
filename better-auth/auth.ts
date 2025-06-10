ab3709d6e2f731cae7badb1c59d68633b76c16af
mujibulhaquetanim <mujibultanim@gmail.com>
2025-06-10 23:59:56 +0600

    feat: enhance authentication setup with Prisma integration and client methods

--- better-auth/auth.ts
+++ better-auth/auth.ts
@@ -1,5 +1,19 @@
 import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+import { PrismaClient } from "@/generated/prisma";
+
+const prisma = new PrismaClient();
  
 export const auth = betterAuth({
-    //...
-})
+    //...other options
+    emailAndPassword: {  
+        enabled: true
+    },
+    socialProviders: { 
+        github: { 
+           clientId: process.env.GITHUB_CLIENT_ID as string, 
+           clientSecret: process.env.GITHUB_CLIENT_SECRET as string, 
+        }, 
+    }, 
+});
