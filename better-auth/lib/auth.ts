9597cf9dd357380c27b77a46d34fffaf93789f69
mujibulhaquetanim <mujibultanim@gmail.com>
2025-06-11 22:14:01 +0600

    feat: initialize Prisma schema with User, Session, Account, and VerificationToken models

--- better-auth/lib/auth.ts
+++ better-auth/lib/auth.ts
@@ -1,13 +1,13 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 // If your Prisma file is located elsewhere, you can change the path
-import { PrismaClient } from "@/generated/prisma";
+import { PrismaClient } from "@/lib/generated/prisma";
 
 const prisma = new PrismaClient();
  
 export const auth = betterAuth({
     database: prismaAdapter(prisma,{
-        provider: "sqlite"
+        provider: "sqlite",
     }),
     
     //...other options
