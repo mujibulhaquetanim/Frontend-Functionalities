48acaa454323e80a25b9c915b95e268bb0289e20
mujibulhaquetanim <mujibultanim@gmail.com>
2025-06-13 23:10:00 +0600

    feat: implement signInAction with Zod validation and redirect to dashboard

--- better-auth/app/(auth)/actions.ts
+++ better-auth/app/(auth)/actions.ts
@@ -2,4 +2,18 @@
 
 import { auth } from "@/lib/auth";
 import { validatedAction } from "@/lib/action-helper";
+import {signinSchema,signupSchema} from "@/lib/types";
+import {redirect} from "next/navigation";
 
+export const signInAction = validatedAction(signinSchema, async (data)=>{
+    const {email, password} = data;
+
+    await auth.api.signInEmail({
+        body: {
+            email,
+            password
+        }
+    });
+
+    redirect("/dashboard");
+});
