6a0b1f7d7fa622fed4b8a981b58ca35abd3eafaa
mujibulhaquetanim <mujibultanim@gmail.com>
2025-07-03 17:04:17 +0600

    fix: correct component name casing, add default case for `message.parts()` and add optional chaining for message parts

--- vercel-ai-sdk/app/page.tsx
+++ vercel-ai-sdk/app/page.tsx
@@ -2,17 +2,19 @@
 import React from "react";
 import { useChat } from "@ai-sdk/react";
 
-export default function page() {
+export default function Page() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
     <div className="flex flex-col w-full max-w-md py-24 mx-auto">
       {messages.map((msg) => (
         <div key={msg.id}>
           {msg.role === "user" ? "You: " + msg.content : "AI: " + msg.content}
-          {msg.parts.map((part, i) => {
+          {msg.parts?.map((part, i) => {
             switch (part.type) {
               case "text":
                 return <div key={`${msg.id}-${i}`}>{part.text}</div>;
+              default:
+                return null;
             }
           })}
         </div>
