65d2cf843a274ca4e61492376645b85153ec0e36
mujibulhaquetanim <mujibultanim@gmail.com>
2025-12-08 23:29:17 +0600

    wrap app with tanstack query provider and send fetch and post req with it using elysiaJs api route

--- elysia-next/src/app/layout.tsx
+++ elysia-next/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
+import { TanstackProvider } from "@/components/QueryProvider";
 
 const jetBrainsMono = JetBrains_Mono({
   variable: "--font-jetbrains-mono",
@@ -20,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${jetBrainsMono.variable} antialiased`}>
-        {children}
+        <TanstackProvider>{children}</TanstackProvider>
       </body>
     </html>
   );
