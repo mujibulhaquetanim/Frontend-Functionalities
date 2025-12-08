28586e6fee9cbc13a2c66f5d7481946e473efb8b
mujibulhaquetanim <mujibultanim@gmail.com>
2025-12-08 22:21:57 +0600

    add new elysia route and fix type naming (PascalCase)

--- elysia-next/src/app/api/[[...slugs]]/route.ts
+++ elysia-next/src/app/api/[[...slugs]]/route.ts
@@ -1,13 +1,21 @@
 import { Elysia, t } from 'elysia'
 
-const app = new Elysia({ prefix: '/api' })
-    .get('/user', {user: {name: 'Hello Mujib, Welcome to ElysiaJS with Next.js 16'}})
+const user = new Elysia()
+    .get('/user', () => ({ user: { name: 'Hello Mujib, Welcome to ElysiaJS with Next.js 16' } }))
     .post('/', ({ body }) => body, {
         body: t.Object({
             name: t.String()
         })
-    })
+    });
 
-export type app = typeof app
-export const GET = app.fetch 
-export const POST = app.fetch 
+const task = new Elysia()
+    .get('/task', () => ({ task: { task: 'customize elysia api for multiple routes' } }));
+
+const app = new Elysia({ prefix: '/api' })
+    .use(user)
+    .use(task);
+
+export const GET = app.fetch;
+export const POST = app.fetch;
+
+export type App = typeof app;
